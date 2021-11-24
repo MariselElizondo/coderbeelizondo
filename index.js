facb15e6e652deee2dc0a4dda77cacfda848b7c1
MariselElizondo <66186983+MariselElizondo@users.noreply.github.com>
2021-11-23 23:26:28 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+ const express = require('express')
+const app = express()
+const port = 8080
+
+let products = [
+  {
+    "title": "Escuadra",
+    "price": 123.45,
+    "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+    "id": 1
+  },
+  {
+    "title": "Calculadora",
+    "price": 234.56,
+    "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
+    "id": 2
+  },
+  {
+    "title": "Globo Terráqueo",
+    "price": 345.67,
+    "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
+    "id": 3
+  }
+ ]
+ 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+app.get('/productos', (req, res) => {
+  res.send('<h1>Productos</h1>', products)
+})
+
+app.get('/productoRandom', (req, res) => {
+  res.send('Productos Random!')
+})
